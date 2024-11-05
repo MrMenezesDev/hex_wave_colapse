bd600f7e3e055ca6dc1deceb39d29c5bb1fc33ac
Erick Menezes <erick.menezes@cubos.io>
2024-11-05 09:37:53 -0300

    refact: organize code

--- hex_grid.py
+++ hex_grid.py
@@ -1,6 +1,7 @@
 import math
 from dataclasses import dataclass, field
 from typing import List, Callable, Tuple
+from math_utils import get_rotation_in_line, line_intersection
 
 @dataclass
 class HexGrid:
@@ -47,6 +48,8 @@ class HexGrid:
                     self.draw_hexagon(center_x, center_y, self.size, color=1)
                 else:
                     self.draw_hexagon(center_x, center_y, self.size)
+                    # Desenhar um hexágono menor no centro
+                    self.draw_hexagon(center_x, center_y, self.size * 0.29, math.pi * 2 / 12, self.colors)
 
     def draw_cell(self, cell):
         """
@@ -65,8 +68,6 @@ class HexGrid:
             edge = cell.tile.edges[i]
             index = [4, -1, 0, 1, 2, 3][i]
             if edge == 1:
-                # Desenhar um hexágono menor no centro
-                self.draw_hexagon(center_x, center_y, self.size * 0.29, math.pi * 2 / 12, self.colors)
                 
                 # Calcular os ângulos dos dois vértices que formam a borda
                 angle1 = math.pi * 2 / 6 * index
@@ -154,35 +155,3 @@ class HexGrid:
 
         return vertices
 
-def get_rotation_in_line(x1, y1, x2, y2, angle):
-    import math
-
-    # Vetor AB (de A para B)
-    AB_x = x2 - x1
-    AB_y = y2 - y1
-
-    # Ângulo de rotação (30 graus convertidos para radianos)
-    angulo_radianos = math.radians(angle)
-
-    # Matriz de rotação
-    cos_theta = math.cos(angulo_radianos)
-    sin_theta = math.sin(angulo_radianos)
-
-    # Vetor AC (rotacionado)
-    AC_x = cos_theta * AB_x - sin_theta * AB_y
-    AC_y = sin_theta * AB_x + cos_theta * AB_y
-
-    # Coordenadas do ponto C
-    x_C = x1 + AC_x
-    y_C = y1 + AC_y
-
-    return x_C, y_C
-
-
-def line_intersection(x1, y1, x2, y2, x3, y3, x4, y4):
-    denom = (x1 - x2) * (y3 - y4) - (y1 - y2) * (x3 - x4)
-    if denom == 0:
-        return None  # Linhas são paralelas
-    px = ((x1 * y2 - y1 * x2) * (x3 - x4) - (x1 - x2) * (x3 * y4 - y3 * x4)) / denom
-    py = ((x1 * y2 - y1 * x2) * (y3 - y4) - (y1 - y2) * (x3 * y4 - y3 * x4)) / denom
-    return px, py
